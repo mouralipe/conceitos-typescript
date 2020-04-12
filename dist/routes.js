deb88616c60a6db462b8ffe60b6bcd191a3da544
mouralipe <felipemoura92@hotmail.com>
2020-04-12 17:01:23 -0300

    ajust

--- dist/routes.js
+++ dist/routes.js
@@ -1,7 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function test(req, res) {
-    return express_1.response.json({ test: "TESTE" });
+    var user = CreateUser_1.default({
+        email: "Felipe",
+        password: "21321",
+        techs: [
+            "Node",
+            "React",
+            "React Native",
+            { title: "Javascript", experience: 100 },
+        ],
+    });
+    return res.json({ test: "TESTE" });
 }
 exports.test = test;
